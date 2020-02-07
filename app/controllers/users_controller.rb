cfec865b0ea682aab97771acb59a72f5c017337c
Ubuntu <ubuntu@ip-172-31-39-224.us-east-2.compute.internal>
2020-02-07 21:46:02 +0000

    added the private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ def new
 end
 
 def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+    @user = User.new(user_params)
   if @user.save
     redirect_to new_user_path
   else
@@ -14,4 +13,9 @@ def create
   end
 end
 
+private
+
+def user_params
+      params.require(:user).permit(:username,:email,:password)
+    end
 end
