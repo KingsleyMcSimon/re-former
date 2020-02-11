9642d2565d5dc9abd281ecc5eab6f09de9dc2217
Roheem Olayemi <abdurroheem20@gmail.com>
2020-02-11 15:59:39 +0100

    Updated users controller linting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,24 +14,22 @@ def create
     render :new
   end
 end
-  
-  def edit
+
+def edit
   @user = User.find(params[:id])
 end
-  
-  def update
+
+def update
   @user = User.find(params[:id])
-  
   if @user.update(user_params)
     redirect_to edit_user_path
   else
     render :edit
   end
 end
-
   private
-  
-  def user_params
+
+def user_params
     params.require(:user).permit(:username,:email,:password)
   end
 end
