f236506230385ac18d1dac66e11bf607d9751a41
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-02-10 21:59:37 -0800

    users_controllers.rb, new.html.erb and the routes.rb files modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  post  'users/new', to: "users#new"
+
   resources :users, only: [:new, :create]
 
 end
