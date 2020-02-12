db6d586e5925df3be1dfc25df0ef73ff895046cb
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-02-11 17:42:03 -0800

    Linter errors corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post  'users/new', to: "users#new"
+  post 'users/new', to: 'users#new'
 
-  get 'users/edit', to: "users#edit"
+  get 'users/edit', to: 'users#edit'
 
-  resources :users, only: [:new, :create, :edit, :update]
-
+  resources :users, only: %i[new create edit update]
 end
